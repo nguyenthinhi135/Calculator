22edba5a7aa5e7e861830a2674e94054c649a1e2
nhinguyenthi <1854050075nhi@ou.edu.vn>
2021-11-03 15:31:04 +0700

    update from1

--- Calculator/Form1.Designer.vb
+++ Calculator/Form1.Designer.vb
@@ -22,10 +22,118 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txta = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtb = New System.Windows.Forms.TextBox()
+        Me.txtkq = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txta
+        '
+        Me.txta.Location = New System.Drawing.Point(292, 109)
+        Me.txta.Margin = New System.Windows.Forms.Padding(4)
+        Me.txta.Name = "txta"
+        Me.txta.Size = New System.Drawing.Size(278, 27)
+        Me.txta.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(163, 109)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Số thứ nhất"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(396, 275)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(163, 170)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Số thứ hai"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(347, 40)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Tính toán"
+        '
+        'txtb
+        '
+        Me.txtb.Location = New System.Drawing.Point(292, 170)
+        Me.txtb.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtb.Name = "txtb"
+        Me.txtb.Size = New System.Drawing.Size(278, 27)
+        Me.txtb.TabIndex = 5
+        '
+        'txtkq
+        '
+        Me.txtkq.Location = New System.Drawing.Point(292, 225)
+        Me.txtkq.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtkq.Name = "txtkq"
+        Me.txtkq.Size = New System.Drawing.Size(278, 27)
+        Me.txtkq.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(172, 228)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Kết quả"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(829, 349)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtkq)
+        Me.Controls.Add(Me.txtb)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txta)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Tính toán"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txta As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtb As TextBox
+    Friend WithEvents txtkq As TextBox
+    Friend WithEvents Label4 As Label
 End Class
